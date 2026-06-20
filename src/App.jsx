75ee80909fbd77ccb551a8bceb738a7bf6e0d905
adityapanwar027 <adityapanwar027@gmail.com>
2026-06-20 15:31:02 +0530

    fix cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 import Dashbord from "./Pages/Dashbord";
 
 function App() {
   const token = localStorage.getItem("token");
+  const [showRegister, setShowRegister] = useState(false);
 
-  return token ? <Dashbord /> : <Login />;
+  if (token) {
+    return <Dashbord />;
+  }
+
+  return (
+    <div>
+      {showRegister ? <Register /> : <Login />}
+
+      <br />
+
+      <button onClick={() => setShowRegister(!showRegister)}>
+        {showRegister ? "Go to Login" : "Go to Register"}
+      </button>
+    </div>
+  );
 }
 
 export default App;
